dd8d3fee832c1be733532a96a845577a569979df
Héctor <hsoriano9986@gmail.com>
2023-11-15 13:17:34 +0100

    JS basico para pantalla winner, pudiendo volver a jugar o volver al inicio

---
+++ js/winner.js
@@ -0,0 +1,10 @@
+let botonPlayAgain = document.getElementById("botonPlayAgain");
+let botonHome = document.getElementById("botonHome");
+
+botonPlayAgain.addEventListener("click", () => {
+    location.href = "./nickname.html";
+});
+
+botonHome.addEventListener("click", () => {
+    location.href = "../index.html";
+});
